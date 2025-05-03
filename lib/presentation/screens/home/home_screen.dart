b64b67819fffaa84ce37fb82986c30b23ac87a4f
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-05-02 23:19:42 -0500

    feat: Add image transparency

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -13,7 +13,11 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Image.asset('assets/images/quiz-logo.png', width: 300),
+              Image.asset(
+                'assets/images/quiz-logo.png',
+                width: 300,
+                color: const Color.fromARGB(150, 255, 255, 255),
+              ),
               const SizedBox(height: 20),
               const Text(
                 'Learn Flutter the fun way!',
