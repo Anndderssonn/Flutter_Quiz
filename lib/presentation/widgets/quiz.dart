cc3bd95127fec36705a710bb50074b59b555ab36
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-05-03 14:51:29 -0500

    feat: Add redirect with ternary condition option

--- lib/presentation/widgets/quiz.dart
+++ lib/presentation/widgets/quiz.dart
@@ -11,17 +11,11 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = HomeScreen(switchScreen);
-    super.initState();
-  }
+  var activeScreen = 'home-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -33,7 +27,10 @@ class _QuizState extends State<Quiz> {
       home: Scaffold(
         body: Container(
           decoration: AppBackground().background(),
-          child: activeScreen,
+          child:
+              activeScreen == 'home-screen'
+                  ? HomeScreen(switchScreen)
+                  : const QuestionsScreen(),
         ),
       ),
     );
